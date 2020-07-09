27f9d3061716838bc3d1598e219455e02abe0a95
Pedro Veloso <pedro.n.veloso@gmail.com>
2020-07-09 16:57:47 -0400

    Add FPS counter to quickly and easily validate the current system setting - no point the user having multiple apps for this

--- app/src/main/java/sszb/s20/refresh/MainActivity.kt
+++ app/src/main/java/sszb/s20/refresh/MainActivity.kt
@@ -1,7 +1,10 @@
 package sszb.s20.refresh
 
 import android.app.Activity
+import android.graphics.Color
 import android.os.Bundle
+import jp.wasabeef.takt.Seat
+import jp.wasabeef.takt.Takt
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -12,6 +15,13 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
         set96.setOnClickListener { setRefreshRate(RefreshRate.NinetySix) }
         set120.setOnClickListener { setRefreshRate(RefreshRate.HundredTwenty) }
+
+        Takt.stock(this.application)
+            .seat(Seat.TOP_RIGHT)
+            .interval(233)
+            .color(Color.BLACK)
+            .size(18f)
+            .alpha(.8f)
     }
 
 
